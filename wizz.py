ff52e16f1e863f4237b5f7c6e9f162b9f7567ae8
jdkok02 <daankok@outlook.com>
2025-08-17 17:52:13 -0400

    Add files via upload

---
+++ wizz.py
@@ -0,0 +1,19 @@
+import asyncio
+from pywizlight import wizlight, PilotBuilder
+
+lamp_ip = '192.168.1.40'           # .40 - 116
+lamp_brightness = 100               # 10 - 250
+
+async def set_lamp(state):
+    lamp = wizlight(lamp_ip)
+    
+    if state == 'on':
+        await lamp.turn_on((PilotBuilder(brightness=lamp_brightness)))
+        print("Lamp is aan.")
+    elif state == 'off':
+        await lamp.turn_off()
+        print("Lamp is uit.")
+
+lamp_status = 'on'
+
+asyncio.run(set_lamp(lamp_status))
